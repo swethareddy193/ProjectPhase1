ab560b1cb64b5131ffa4a49d19ac3740cf4f738e
swethareddy193 <swethar839@gmail.com>
2020-12-17 06:31:30 +0000

    projectphase1

---
+++ Project/Phase1project/src/phase1/Filecreation.java
@@ -0,0 +1,18 @@
+package phase1;
+
+import java.io.File;
+
+public class Filecreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void Createfile(String file,String data) {
+		
+		File newfile=new File(file);
+		
+		
+	}
+
+}
